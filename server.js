9a83eec66b73e711c44aafc03ac7a2fc9ff67f43
Felix0291 <felix.tapper@gmail.com>
2025-05-09 11:26:55 +0200

    add category route + module

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -18,11 +19,14 @@ app.use(express.json());
 // Routes
 const productsRouter = require("./routes/products");
 app.use("/api/products", productsRouter);
+const categorysRouter = require("./routes/category");
+app.use("/api/category", categorysRouter);
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
+  res.status(err.status || 500).json({ error: err.message || "Something went wrong!" });
 });
 
 app.listen(PORT, () => {
